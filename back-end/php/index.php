a4edbb9b60b6d311e28c8070b718dae92bead1e7
leandro-hbs <leandro.batista@academico.ifpb.edu.br>
2019-12-17 15:03:32 -0300

    Conteiner com Mysql e Apache funcionando

--- back-end/php/index.php
+++ back-end/php/index.php
@@ -1,11 +1,7 @@
 <?php
-  $id = $_GET['id'] ?? 'null';
-  $json_file = file_get_contents('../database/db.json');
-  $dados = json_decode($json_file,true);
-  foreach ( $dados['dispositivos']["$id"] as $dado ){
+  $consulta = "SELECT * FROM hosts";
+  $con      = $mysqli->query($consulta) or die($mysqli->error);
+  foreach( $con as $dado){
     echo $dado;
   }
-  $result = [];
-  $result['response'] = shell_exec("echo '$conf' | sshpass -p '$senha' ssh -o 'StrictHostKeyChecking no' $nome@$address");
-  echo json_encode($result);
 ?>
