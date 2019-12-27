13189fa8e63072991fb5803fc81734c53884d5c9
leandro-hbs <leandro.batista@academico.ifpb.edu.br>
2019-12-27 17:22:44 -0300

    Inserindo pagina principal a partir do mysql

--- back-end/js/script.js
+++ back-end/js/script.js
@@ -5,23 +5,27 @@ function buscaDisp() {
 }
 
 function inserir(dispositivos){
+    for (let disp of dispositivos) {
+        inserirDisp(disp)
+    }
+}
+
+function inserirDisp(disp){
     let areaDisp = document.querySelector("#area-disp");
     let rows = "";
-    for (let disp of dispositivos) {
-        rows += `<tr>
-        <td>${disp.id}</td>
-        <td>${disp.nome}</td>
-        <td>${disp.tipo}</td>
-        <td>${disp.endereco}</td>
-        <td>
-            <button type="button" id='${disp.id}' onclick='pageConfig()'>
-                <i class="fas fa-cogs"></i>
-            </button>
-            <button type="button" id='${disp.id}' onclick='deletaDisp()'>
-                <i class="fas fa-trash-alt"></i>
-            </button>
-        </tr>`;
-    }
+    rows += `<tr>
+    <td>${disp.id}</td>
+    <td>${disp.nome}</td>
+    <td>${disp.tipo}</td>
+    <td>${disp.endereco}</td>
+    <td>
+        <button type="button" id='${disp.id}' onclick='pageConfig()'>
+            <i class="fas fa-cogs"></i>
+        </button>
+        <button type="button" id='${disp.id}' onclick='deletaDisp()'>
+            <i class="fas fa-trash-alt"></i>
+        </button>
+    </tr>`;
     areaDisp.innerHTML = rows;
 }
 
