908008d66a04d6e8dfe1373127a55b0c90846f0d
leandro-hbs <leandro.batista@academico.ifpb.edu.br>
2020-01-16 15:22:20 -0300

    Informaçoes adicionadas na segunda pagina

--- back-end/database/db.sql
+++ back-end/database/db.sql
@@ -9,7 +9,7 @@ CREATE TABLE hosts (
   tipo varchar(100) NOT NULL,
   PRIMARY KEY (id)
 );
-INSERT INTO hosts VALUES (1, 'aluno', 'aluno', '127.16.0.70', 'Host');
+INSERT INTO hosts VALUES (1, 'aluno', 'aluno', '172.16.0.70', 'Host');
 INSERT INTO hosts VALUES (2, 'S0', '*****', '-.-.-.-', 'Switch');
 INSERT INTO hosts VALUES (3, 'DNS', '*****', '200.10.10.10', 'Server');
 INSERT INTO hosts (nome, senha, endereco, tipo) VALUES
--- back-end/js/config.js
+++ back-end/js/config.js
@@ -1,27 +1,34 @@
 function buscaDisp() {
   fetch("http://localhost:8080/back-end/php/p2/busca.php?id=1")
     .then(res => res.json())
-    .then(json => verifica(json));
+    .then(json => informacoes(json));
 }
 
-function verifica(dispositivo){
+function informacoes(dispositivo){
   fetch(`http://localhost:8080/back-end/php/p2/info.php?ip=${dispositivo[0].endereco}&nome=${dispositivo[0].nome}&senha=${dispositivo[0].senha}`)
     .then(res => res.json())
-    .then(json => insere(json));
+    .then(json => inserirInfo(json));
 }
 
-function insere(dados){
-  for (let dado of dados) {
-    inserirInfo(dado[0][0],dado[0][1])
-  }
-}
-
-function inserirInfo(info, dado){
+function inserirInfo(dados){
+  console.log(dados)
   let infoDisp = document.querySelector("#info-disp");
   let rows = "";
   rows += `<tr>
-  <td>${info}</td>
-  <td>${dado}</td>
+  <td>Processador</td>
+  <td>${dados[0]}</td>
+  </tr>
+  <tr>
+  <td>Placa de Video</td>
+  <td>${dados[1]}</td>
+  </tr>
+  <tr>
+  <td>Memória Total</td>
+  <td>${dados[2]}</td>
+  </tr>
+  <tr>
+  <td>Memória Livre</td>
+  <td>${dados[3]}</td>
   </tr>`;
   infoDisp.innerHTML += rows;
 }
